8076a5124c1b2c79819295aaa76d78baca07f42e
melihefe21 <990713115@mydavinci.nl>
2022-09-13 13:46:00 +0200

    module 2

---
+++ module 2.rtfd/1__#$!@%!#__feestlunch.py
@@ -0,0 +1,9 @@
+crossaint = 17.0
+prijs = 0.39
+stokbroden = 2.0
+prijs_2 = 2.78
+korting = 3.0
+kortingsprijs = 0.50
+
+print(f'{stokbroden * prijs_2 + crossaint * prijs - korting * kortingsprijs} met korting')
+print(f'{stokbroden * prijs_2 + crossaint * prijs} zonder korting')
