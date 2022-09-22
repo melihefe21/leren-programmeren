286b55bbb33a9a6facf8794fcc40746dda64ffbc
melihefe21 <990713115@mydavinci.nl>
2022-09-22 09:42:43 +0200

    smartphone.py

---
+++ smartphone.py
@@ -0,0 +1,17 @@
+iphone = 888
+samsung = 780
+
+
+print(f'De iphone is het duurst, de telefoon kost: {iphone} euro')
+print(f'De samsung is het goedkoopst,de telefoon kost: {samsung} euro')
+print(f'Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} euro goedkoper/duurder dan de iphone')
+
+
+
+
+
+
+
+
+# print(f'iphone kost{iphone} euro en de samsung kost{samsung} euro')
+# print (f'{iphone - samsung} is het verschil tussen de twee')
