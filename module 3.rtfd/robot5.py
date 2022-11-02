211971a48adca3422165b595f26c05fe46f95d20
melihefe21 <990713115@mydavinci.nl>
2022-11-02 13:39:54 +0100

    robot5

---
+++ module 3.rtfd/robot5.py
@@ -0,0 +1,19 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 5')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.speed=4
+for x in range(1,8):
+    robotArm.moveRight()
+for x in range(7):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
