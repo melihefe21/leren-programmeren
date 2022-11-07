dcee968459be74be8e566a066b89b96063d2b871
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2022-11-07 12:07:52 +0100

    som1000

--- module 3.rtfd/herhaaldelijk.py
+++ module 3.rtfd/herhaaldelijk.py
@@ -1,7 +1,22 @@
-quit = input("?")
-aantal = 0
 
-while quit != "quit":
-    quit = input("?")
-    aantal += 1
-print(aantal)
+
+getal = 50
+som = 50
+layout = '50 '
+
+while som < 1000:
+    getal += 1
+    som += getal
+    layout += f'+ {getal} '
+    print(f'{layout} = {som}')
+
+
+
+
+# quit = input("?")
+# aantal = 0
+
+# while quit != "quit":
+#     quit = input("?")
+#     aantal += 1
+# print(aantal)
