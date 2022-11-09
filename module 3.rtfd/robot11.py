93304f6d13ee40d4f07add795eba2da1a35dcaf2
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2022-11-09 22:42:07 +0100

    robot11

---
+++ module 3.rtfd/robot11.py
@@ -0,0 +1,14 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 11')
+for i in range(9):
+    robotArm.grab()
+    robotArm.scan()
+    if robotArm.scan() == 'white':
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveRight()
+    else:
+        robotArm.drop()
+        robotArm.moveRight()
+robotArm.wait()
