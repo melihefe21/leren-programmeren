b35bfeac1366ba5127d7015b83c2428eba8fc049
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2022-11-24 09:56:32 +0100

    dagen

---
+++ module 4.rtfd/dagen.py
@@ -0,0 +1,12 @@
+dagen = ['maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag']
+for x in range (0,5):
+    print(dagen[x])
+print('')
+for x in range (5,7):
+    print(dagen[x])
+print('')
+for x in range(4,-1,-1):
+    print(dagen[x])
+print('')
+for x in range(6,4,-1):
+    print(dagen[x])
