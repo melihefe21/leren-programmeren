efb05faac4d54f9494f83794e4f5800a21eafb8c
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2023-01-13 09:39:22 +0100

    mod

---
+++ module 2/groot.py
@@ -0,0 +1,12 @@
+from re import A
+
+
+a = 3
+b = 4
+
+if a >= b:
+    max = a 
+    print(f'het grootste getal is {max}')
+elif a < b:
+    max = a
+    print(f'a is het kleinste getal {max}.')
---
+++ module 2/welke_is_groter.py
@@ -0,0 +1,9 @@
+
+
+
+A = 10
+B = 5
+
+if A >= B :
+   max = A
+print (f' het grootste getal is {max}')
