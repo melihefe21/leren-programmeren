1f2e8086804e2e02ef2d8cf000780d48bc26258f
melihefe21 <113095699+melihefe21@users.noreply.github.com>
2024-09-19 13:36:03 +0200

    ssaverino

--- mpa/musicplayer/resources/views/songs/index.blade.php
+++ mpa/musicplayer/resources/views/songs/index.blade.php
@@ -1,7 +1,8 @@
 @extends("layouts.master")
 
 @section("content")
-    <h1>Hier is 1 lijst met alle liedjes</h1>
+
+    <h1>Hier is 1 lijst met alle liedjes....{{Auth::user()->name}}</h1>
     <ul>
         @foreach ($songs as $song)
             <li>{{$song->name}} - {{$song->genre->name}}</li>
--- mpa/musicplayer/routes/web.php
+++ mpa/musicplayer/routes/web.php
@@ -1,29 +1,31 @@
-<!--php
+<?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SongController;
 use App\Http\Controllers\Welcome;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [Welcome::class, 'hello']);
-Route::get('/songs', [SongController::class, 'index']); -->
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
-
-<?php
-use App\Http\Controllers\PlaylistController;
-use App\Http\Controllers\ViewsController;
-use App\Http\Controllers\Welcome;
-use Illuminate\Support\Facades\Route;
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/', function () {
     return view('products/index');
 });
 Route::get("/hello", [welcome::class, "hello"]);
+Route::get('/hello', [Welcome::class, 'hello']);
+Route::get('/songs', [SongController::class, 'index']);
+
 
 Route::get("/hello", [ViewsController::class, "hello"])->name('hello');
 
@@ -37,3 +39,5 @@ Route::get("/views", [ViewsController::class, "index"]);
 Route::get("/playlist/all", [PlaylistController::class,"index"]);
 Route::get("/playlist/view/{playlist}", [PlaylistController::class,"show"]);
 Route::post("/playlist/addsong/{playlist}", [PlaylistController::class, "AddSongtoPlaylist"]);
+
+require __DIR__.'/auth.php';
